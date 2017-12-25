5f479b7a9f629b18b7e56fe92c611b8dc539c8a6
Satwik Singh <satwik@Satwiks-MacBook-Pro.local>
2017-12-25 23:26:52 +0530

    UniqueSub

--- LongestUniqueSubstring.py
+++ LongestUniqueSubstring.py
@@ -8,4 +8,27 @@
 #
 #Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 # return type must be an int signifying the length of the unique substring 
-def longestUniqueSub()
+def isUnique(s):
+	myDict = {}
+	for c in s:
+		myDict[c] += 1
+	for key in myDict:
+		if myDict[key] > 1:
+			return False
+	return True
+
+
+def longestUniqueSub(s):
+	if len(s) <= 1:
+		return s
+	if isUnique(s):
+		return s
+	else:
+		temp1 = longestUniqueSub(s[1:])
+		temp2 = longestUniqueSub(s[:1])
+		if len(temp1)>len(temp2):
+			return temp1
+		else:
+			return temp2
+
+	
