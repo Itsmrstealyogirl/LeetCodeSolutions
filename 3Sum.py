9c9c0f765aa3b37356283562a2d303ba0148d798
Satwik Singh <satwik@Satwiks-MacBook-Pro.local>
2017-12-26 01:27:20 +0530

    Finished 3Sum

--- 3Sum.py
+++ 3Sum.py
@@ -4,7 +4,26 @@ def threeSum(nums):
         :rtype: List[List[int]]
         """
         retList = []
-        hi = len(nums)-1
-        iter8 = 1
+        myDict = {}
         nums.sort()
-        for lo in range(len(nums)) and hi in range(len(nums) - 1, lo, -1):
+        for lo in range(len(nums)-2):
+        	hi = len(nums)-1
+        	iter8 = lo + 1
+        	while iter8 < hi:
+        		arr = [nums[lo],nums[iter8],nums[hi]]
+        		arr.sort()
+        		if sum(arr) == 0:
+        			if str(arr) in myDict:
+        				myDict[str(arr)] += 1
+        				iter8 += 1
+        				hi -= 1
+        			else: 
+        				retList.append(arr)
+        				myDict[str(arr)] = 1
+        				iter8 += 1
+        				hi -= 1
+        		if sum(arr) > 0:
+        			hi -= 1
+        		if sum(arr) < 0:
+        			iter8 += 1
+        return retList
