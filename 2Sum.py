dd44a3b29b6f3704978161f36418094ebc0eec14
Satwik Singh <satwik@Satwiks-MacBook-Pro.local>
2017-12-27 07:02:54 +0530

    accepted 2sum

--- 2Sum.py
+++ 2Sum.py
@@ -1,4 +1,5 @@
 def twoSum(nums, target):
+    temp = nums[:]
     nums.sort()
     hi = len(nums) - 1
     lo = 0
@@ -8,5 +9,8 @@ def twoSum(nums, target):
         elif (nums[lo] + nums[hi]) > target:
             hi -= 1
         else:
-            return [lo, hi]
+            if nums[lo] == nums[hi]:
+                return [temp.index(nums[lo]), temp[temp.index(nums[lo])+1:].index(nums[hi])+temp.index(nums[lo])+1]
+            else:
+                return [temp.index(nums[lo]), temp.index(nums[hi])]
 
