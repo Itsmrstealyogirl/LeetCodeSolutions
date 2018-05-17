a0ebb7449122b9a09e96434a707cdee968b18787
satwik <satwik.illini@gmail.com>
2018-05-16 19:45:42 -0500

    Added gitignore and changed approach to LCS

--- LongestUniqueSubstring.py
+++ LongestUniqueSubstring.py
@@ -1,34 +1,24 @@
-#Given a string, find the length of the longest substring without repeating characters.
+# Given a string, find the length of the longest substring without repeating characters.
 #
-#Examples:
+# Examples:
 #
-#Given "abcabcbb", the answer is "abc", which the length is 3.
+# Given "abcabcbb", the answer is "abc", which the length is 3.
 #
-#Given "bbbbb", the answer is "b", with the length of 1.
+# Given "bbbbb", the answer is "b", with the length of 1.
 #
-#Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
-# return type must be an int signifying the length of the unique substring 
+# Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must
+#  be a substring, "pwke" is a subsequence and not a substring.
+# return type must be an int signifying the length of the unique substring
+
+
 def isUnique(s):
-	myDict = {}
-	for c in s:
-		if c not in myDict:
-			myDict[c] = 1
-		else:
-			return False
-	return True
+    return len(s) == len(set(s))
 
 
 def longestUniqueSub(s):
-	if len(s) <= 1:
-		return s
-	if isUnique(s):
-		return s
-	else:
-		temp1 = longestUniqueSub(s[1:])
-		temp2 = longestUniqueSub(s[:1])
-		if len(temp1)>len(temp2):
-			return temp1
-		else:
-			return temp2
+    if len(s) <= 1:
+        return s
+    if isUnique(s):
+        return s
+    else:
 
-	
