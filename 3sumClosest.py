83038769e04ef9332f13af47de50195616c38c59
satwik <satwik.illini@gmail.com>
2018-05-19 23:59:08 -0500

    Daily check, working on 3sumclosest

--- 3sumClosest.py
+++ 3sumClosest.py
@@ -2,4 +2,10 @@
 ## Return the 3sum that is closest to the target
 
 def 3sumClosest(nums, target):
-    
+	nums.sort()
+	for head in range(len(nums) - 2):
+		tail = len(nums) - 1
+		iter8 = head + 1
+		sum1 = nums[tail] + nums[head] + nums[iter8]
+		while iter8 < tail:
+			
