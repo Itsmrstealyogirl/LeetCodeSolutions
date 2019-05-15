60a5c3b67a4bf9960cc62d5e50d71a1e24da6e33
satwik <satwik.illini@gmail.com>
2019-05-15 00:37:32 -0500

    ACCEPTED

--- most_water.py
+++ most_water.py
@@ -20,21 +20,37 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        return 0
+        return better_solution(height)
 
 
 
 
 def naive_solution(list_n):
-	area_list = []
-	for i in range(len(list_n)):
-		for j in range(i+1, len(list_n)):
-			area_list.append(min(list_n[i], list_n[j]) * (j - i))
-	return max(area_list)
+    area_list = []
+    for i in range(len(list_n)):
+        for j in range(i+1, len(list_n)):
+            area_list.append(min(list_n[i], list_n[j]) * (j - i))
+    return max(area_list)
 
-print(naive_solution([1,8,6,2,5,4,8,3,7]))
+def naive_dp(height):
+    area_dict = {}
+
+
 
 
 def better_solution(height):
-	area_list = []
-	return max(area_list)
+    start_index = 0
+    end_index = len(height) - 1
+    max_area = min(height[start_index], height[end_index]) * (end_index - start_index)
+
+    while (start_index < end_index):
+        if (height[start_index] > height[end_index]):
+            end_index -= 1
+        else:
+            start_index += 1
+
+        max_area = max(max_area, min(height[start_index], height[end_index]) * (end_index - start_index))
+    return max_area
+
+print(better_solution([1,8,6,2,5,4,8,3,7]))
+print(better_solution([1,7,6,7,5,2,4]))
